ec27e8e980f92791748e609115305d656c27792c
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-22 12:20:40 -0700

    Added virtual for thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,26 +1,34 @@
 const { Schema, model } = require("mongoose");
 
 //Creating model for thoughts
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    //use getter method to format the timestamp on query
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Reaction",
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
     },
-  ],
-});
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //use getter method to format the timestamp on query
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Reaction",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
