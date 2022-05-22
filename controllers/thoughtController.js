8606bfd65f597c3889833fd18ed513b4a50b181a
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-22 15:34:57 -0700

    Added route to get a single thouhgt and to create a new thought in thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,4 +7,24 @@ module.exports = {
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
+  // Get a single thought by id
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : res.json(course)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a new thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => {
+        console.log(err);
+        return res.stauts(500).json(err);
+      });
+  },
 };
