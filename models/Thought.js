e8a3b4c85ebc548477035707fcf7b03199c07a2b
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-22 12:17:36 -0700

    Created the thoughts model

---
+++ models/Thought.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+//Creating model for thoughts
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    //use getter method to format the timestamp on query
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Reaction",
+    },
+  ],
+});
