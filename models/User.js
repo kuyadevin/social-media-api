c0c9c81b82840ad03df0385d1a129fef2f05c906
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-22 11:49:53 -0700

    Added model and api routes base files and started making user model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const { Schema, Types } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    // new to add trim aggretgate to server.js
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+      },
+    },
+  },
+  thoughts: {
+    type: Schema.Types.ObjectId,
+  },
+});
