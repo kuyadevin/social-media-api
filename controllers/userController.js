8ffbe21cad61fbb3f5d8230af5a398be23eff6a5
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-22 14:51:47 -0700

    Added routes to get all users and to get a user by id

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  //Get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(courses))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a specific user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
