dbf5ea17bfba4dc5206538cf2e69be3ca6d9fac5
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-22 15:06:14 -0700

    Added route to delete a user and their thoughts, finsihing the userCOntroller page

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,4 +41,15 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  // Delete a user
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : Thoughts.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() => res.json({ message: "User and thoughts deleted!" }))
+      .catch((err) => res.status(500).json(err));
+  },
 };
